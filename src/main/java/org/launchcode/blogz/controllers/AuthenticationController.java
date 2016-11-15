9fd3ca05de2250f3605b971603eb68dcfd749402
abarla <haritha.manchili@gmail.com>
2016-11-14 21:11:30 -0600

    Commit for Blogz-Spring

--- src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
+++ src/main/java/org/launchcode/blogz/controllers/AuthenticationController.java
@@ -11,75 +11,92 @@ import org.springframework.web.bind.annotation.RequestMethod;
 
 @Controller
 public class AuthenticationController extends AbstractController {
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.GET)
 	public String signupForm() {
 		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/signup", method = RequestMethod.POST)
 	public String signup(HttpServletRequest request, Model model) {
-		// TODO - implement signup
-				// get parameters from request object
-				// validate parameters (username, password, verify)
-				// if validated, create new user, and put them in the session
-				// access session...   Session thisSession = request.getSession(); (in AbstractController)
-				
 		String newUsername = request.getParameter("username");
 		model.addAttribute("username", newUsername);
 		String newPassword = request.getParameter("password");
 		String newVerifyPassword = request.getParameter("verify");
-//		String user_error = request.getParameter("username_error");
-//		user_error = "Not a valid username";
-//		String pass_error = request.getParameter("password_error");
-//		pass_error = "Not a valid password";
-//		String v_error = request.getParameter("verify_error");
-//		v_error = "password not matching";
-		
-		
+
+		if (!User.isValidUsername(newUsername)){
+			String username_error = "Bad name";
+			model.addAttribute("username_error", username_error);
+		}
+		if (!User.isValidPassword(newPassword)){
+			String password_error = "Bad password";
+			model.addAttribute("password_error", password_error);
+		}
+		if (!newVerifyPassword.equals(newPassword)){
+			String verify_error = "Your passwords do not match";
+			model.addAttribute("verify_error", verify_error);
+		}
 		if ((User.isValidUsername(newUsername)) && (User.isValidPassword(newPassword))){
 			if (newVerifyPassword.equals(newPassword)){
 				User newUser = new User(newUsername, newPassword);
 				userDao.save(newUser);
 				HttpSession thisSession = request.getSession();
 				setUserInSession(thisSession, newUser);
-				
+
 				return "redirect:blog/newpost";
 			}
-			
-		}	
-		
+
+		}		
+
+		// TODO - implement signup
+		// get parameters from request object
+		// validate parameters (username, password, verify)
+		// if validated, create new user, and put them in the session
+		// access session...   Session thisSession = request.getSession(); (in AbstractController)
+
+
 		return "signup";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.GET)
 	public String loginForm() {
 		return "login";
 	}
-	
+
 	@RequestMapping(value = "/login", method = RequestMethod.POST)
 	public String login(HttpServletRequest request, Model model) {
-		
+
 		// TODO - implement login
-		// get parameters from request
-		
+		// get parameters from request		
 		// get user by their username User.username
-		
+
 		//check password is correct, if not correct password redirect to the form and give an error message
-		
+
 		// log them in, if so (i.e. setting the user in the session
-		
+
 		String newUsername = request.getParameter("username");
 		String newPassword = request.getParameter("password");
-		//if (User.isMatchingPassword(newPassword))
-		
-		request.getSession();
+		User user = userDao.findByUsername(newUsername);
+		// no user exists error
+		if (user == null){
+			String error = "Your user name does not exist";
+			model.addAttribute("error", error);
+			return "login";
+		}
+		if (!user.isMatchingPassword(newPassword)){
+			String error = "Try again";
+			model.addAttribute("error", error);
+			return "login";
+
+		}
+		HttpSession thisSession = request.getSession();
+		setUserInSession(thisSession, user);
 		return "redirect:blog/newpost";
 	}
-	
+
 	@RequestMapping(value = "/logout", method = RequestMethod.GET)
 	public String logout(HttpServletRequest request){
-        request.getSession().invalidate();
+		request.getSession().invalidate();
 		return "redirect:/";
 	}
 }
--- src/main/java/org/launchcode/blogz/controllers/PostController.java
+++ src/main/java/org/launchcode/blogz/controllers/PostController.java
@@ -19,29 +19,75 @@ public class PostController extends AbstractController {
 	public String newPostForm() {
 		return "newpost";
 	}
-	
+
 	@RequestMapping(value = "/blog/newpost", method = RequestMethod.POST)
 	public String newPost(HttpServletRequest request, Model model) {
-		
+
 		// TODO - implement newPost
+
+		//get request parameters
+		String newTitle = request.getParameter("title");
+		String newBody = request.getParameter("body");
+		User author = getUserFromSession(request.getSession());
+
+		// validate parameters
+		// if not valid, send back to form with error message
+		//if valid,create new Post
 		
-		return "redirect:index"; // TODO - this redirect should go to the new post's page  		
+		if (newTitle.equals("") || newTitle.equals(null)){
+			String error = "Hey turd, you need a title";
+			model.addAttribute("error", error);
+			return "newpost";
+		}
+
+		
+		if (newBody.equals("") || newBody.equals(null)){
+			String error = "Hey turd, you need a body";
+			model.addAttribute("error", error);
+			return "newpost";
+		}
+		
+			Post newPost = new Post(newTitle, newBody, author);
+			postDao.save(newPost);
+
+			String username = author.getUsername();
+			int uid = newPost.getUid();
+			return singlePost(username, uid, model);
+			
+
+
+		//return "redirect:index"; // TODO - this redirect should go to the new post's page  		
 	}
-	
+
+	// handle requests like /blog/chris/5
+
 	@RequestMapping(value = "/blog/{username}/{uid}", method = RequestMethod.GET)
 	public String singlePost(@PathVariable String username, @PathVariable int uid, Model model) {
-		
+
 		// TODO - implement singlePost
-		
+
+		//get given post
+		Post newPost = postDao.findByUid(uid);
+
+		// pass post into the template
+		model.addAttribute("post", newPost);
+
 		return "post";
 	}
-	
+
 	@RequestMapping(value = "/blog/{username}", method = RequestMethod.GET)
 	public String userPosts(@PathVariable String username, Model model) {
-		
+
 		// TODO - implement userPosts
-		
+
+		// get all user posts
+		User user = userDao.findByUsername(username);
+		List<Post> userPosts = user.getPosts();
+
+		// pass the posts into the template
+		model.addAttribute("posts", userPosts);
+
 		return "blog";
 	}
-	
+
 }
