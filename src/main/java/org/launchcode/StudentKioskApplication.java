5730a00bb422fad9b814f3144c32c73f8aaa14de
abarla <haritha.manchili@gmail.com>
2016-11-21 13:03:31 -0600

    commit

--- src/main/java/org/launchcode/StudentKioskApplication.java
+++ src/main/java/org/launchcode/StudentKioskApplication.java
@@ -4,9 +4,9 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class BlogzSpringApplication {
+public class StudentKioskApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(BlogzSpringApplication.class, args);
+		SpringApplication.run(StudentKioskApplication.class, args);
 	}
 }
